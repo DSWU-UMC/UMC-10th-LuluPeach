d3738ce20932d35b6f4fb7bad6b1d369cbace24d
lilyyang0077 <lilyyang0077@test.com>
2026-04-29 22:09:24 +0900

    controller, DTO, ErrorCode

--- src/main/java/com/example/umc10th/domain/member/entity/Member.java
+++ src/main/java/com/example/umc10th/domain/member/entity/Member.java
@@ -1,4 +1,18 @@
 package com.example.umc10th.domain.member.entity;
 
+import java.util.prefs.Preferences;
+
 public class Member {
+    public static Preferences builder() {
+    }
+
+    public String getName() {
+    }
+
+    public Integer getPoint() {
+    }
+
+    public Long getId() {
+        return null;
+    }
 }
