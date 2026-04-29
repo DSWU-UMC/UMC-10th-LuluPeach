3749368f75c4e68178e9ab6fc4baa49661e37b9e
lilyyang0077 <lilyyang0077@test.com>
2026-04-29 22:09:24 +0900

    controller, DTO, ErrorCode

--- src/main/java/com/example/umc10th/domain/member/service/MemberService.java
+++ src/main/java/com/example/umc10th/domain/member/service/MemberService.java
@@ -1,4 +1,27 @@
 package com.example.umc10th.domain.member.service;
 
+import com.example.umc10th.domain.member.converter.MemberConverter;
+import com.example.umc10th.domain.member.dto.*;
+import com.example.umc10th.domain.member.entity.Member;
+import com.example.umc10th.domain.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class MemberService {
+
+
+    //마이페이지에 필요한 QueryParameter 처리하는 코드
+    public String singleParameter(String singleParameter) {
+        return singleParameter;
+    }
+
+    //회원가입에 필요한 RequestBody 처리하는 부분
+    public MemberResDTO.RequestBody requestBody(
+            MemberReqDTO.RequestBodyClass dto
+    ){
+        return MemberConverter.toRequestBody(dto.toRequestBody(), dto.longTest());
+    }
+    //질문! 여기 위에 toRequestBody, toRequestBody(), longTest() 는 아직 안 만들었으니 오류 나는 게 정상 맞죠??
 }
--- src/main/java/com/example/umc10th/domain/review/controller/ReviewController.java
+++ src/main/java/com/example/umc10th/domain/review/controller/ReviewController.java
@@ -1,4 +1,26 @@
 package com.example.umc10th.domain.review.controller;
 
+import com.example.umc10th.domain.member.dto.MemberReqDTO;
+import com.example.umc10th.domain.member.dto.MemberResDTO;
+import com.example.umc10th.domain.review.dto.ReviewReqDTO;
+import com.example.umc10th.domain.review.dto.ReviewResDTO;
+import com.example.umc10th.domain.review.service.ReviewService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api")
+
 public class ReviewController {
+    //리뷰 작성 관련 controller
+    @PostMapping("/store/{storeId}/review")
+    public ReviewResDTO.RequestBody requestBody(
+            @RequestBody ReviewReqDTO.RequestBodyClass dto
+    ) {
+        return ReviewService.requestBody(dto);
+    }
 }
