e109a9546ba0162f3d04e02e3116ea79076bb813
lalahyun <lilyyang0077@gmail.com>
2026-05-27 21:09:18 +0900

    Revert "[Week/#9] 9주차 워크북 미션"

--- src/main/java/com/example/umc10th/global/config/SecurityConfig.java
+++ src/main/java/com/example/umc10th/global/config/SecurityConfig.java
@@ -1,9 +1,5 @@
 package com.example.umc10th.global.config;
 
-import com.example.umc10th.global.security.filter.JwtAuthFilter;
-import com.example.umc10th.global.security.service.CustomUserDetailService;
-import com.example.umc10th.global.security.util.JwtUtil;
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -13,20 +9,11 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
-
-import com.example.umc10th.global.security.exception.CustomAccessDenied;
-import com.example.umc10th.global.security.exception.CustomEntryPoint;
 
 // 보안 규칙이 해당 코드임을 알려주는 어노테이션
 @EnableWebSecurity
 @Configuration
-@RequiredArgsConstructor
 public class SecurityConfig {
-
-    private final JwtUtil jwtUtil;
-    private final CustomUserDetailService customUserDetailService;
 
     private final String[] allowUris = {
             // Swagger 허용
@@ -50,24 +37,15 @@ public class SecurityConfig {
                         .anyRequest().authenticated() // 그 외 API는 전부 로그인 필요
                 )
                 //Spring Security 기본 로그인 시스템 사용겠하겠다는 뜻
-                .formLogin(AbstractHttpConfigurer::disable)
-
-                // 세션로그인 방지
-                .sessionManagement(AbstractHttpConfigurer::disable)
-
-                // jwt 필터
-                .addFilterBefore(jwtAuthFilter(), UsernamePasswordAuthenticationFilter.class)
+                .formLogin(form -> form
+                        .defaultSuccessUrl("/swagger-ui/index.html", true)
+                        .permitAll()
+                )
                 // 로그아웃
                 .logout(logout -> logout
                         .logoutUrl("/logout")
                         .logoutSuccessUrl("/login?logout")
                         .permitAll()
-                )
-
-                // 예외상황 핸들러
-                .exceptionHandling(exception -> exception
-                        .accessDeniedHandler(customAccessDenied())
-                        .authenticationEntryPoint(customEntryPoint())
                 );
 
         return http.build();
@@ -80,22 +58,5 @@ public class SecurityConfig {
 
         return NoOpPasswordEncoder.getInstance();
     }
-
-    @Bean
-    public JwtAuthFilter jwtAuthFilter() {
-        return new JwtAuthFilter(jwtUtil, customUserDetailService);
-    }
-
-    @Bean
-    public CustomAccessDenied customAccessDenied() {
-        return new CustomAccessDenied();
-    }
-
-    @Bean
-    public CustomEntryPoint customEntryPoint() {
-        return new CustomEntryPoint();
-    }
 }
-
-
 
